d97c4e0c7cfab3c526e50b9a26cf6cac246fdb04
Gromite <68029797+Gromite@users.noreply.github.com>
2022-12-28 16:11:49 +0100

    merge

--- js/gui.js
+++ js/gui.js
@@ -30,19 +30,46 @@ function preload(){
 function setup() {
   frameRate(100)
   createCanvas(w,h);
-  rectMode(CENTER);
 
 
-  // Synth Inputs
+  // --- Get as input the values of the Onsets and Pulses of the Tracks.
+  gen_button = createButton('⚡');
+  gen_button.position(3.5*w/12, h/3);
+  gen_button.style('background-color', '#878F9B');
+  gen_button.style('border-color', '#878F9B');
+  gen_button.style('border-radius' , 10 + '%');
+  gen_button.style('z-index',  100);
+  gen_button.mousePressed(function() {
+    clear()
+
+    stop_aud();
+    phaseShiftAmount = parseInt(phaseShiftAmountInp.value());                   // How many pulses is each shift
+    phaseShiftPeriod = parseInt(phaseShiftAmountInp.value());                   // After how many bars does a shift occur
+    length = parseInt(lengthInp.value());                            // Length of total piece
+    numberOfTracks = parseInt(numberOfTracksInp.value());
+    onsetsA = parseInt(inp1.value());
+    pulsesA = parseInt(inp2.value());
+    onsetsB = parseInt(inp3.value());
+    pulsesB = parseInt(inp4.value());
+    generateMidi(onsetsA, pulsesA, onsetsB, pulsesB);
+
+    gen_button.style('background-color', '#02139b')
+  });
+
+  // ---- Selection of the type of Synth according to the user
+
   tr1 = createSelect();
-  tr1.position(110, 10);
+  tr1.position(11*w/12,h/10);
+
   tr2 = createSelect();
-  tr2.position(110, 30);
+  tr2.position(11*w/12,h/10+20);
 
   tr3 = createSelect();
-  tr3.position(110, 50);
+  tr3.position(11*w/12,h/10+40);
+
   tr4 = createSelect();
-  tr4.position(110, 70);
+  tr4.position(11*w/12,h/10+60);
+
   for( i = 0; i<All_Synths.length;i++){
     tr1.option(All_Synths[i]);
     tr2.option(All_Synths[i]);
@@ -54,70 +81,38 @@ function setup() {
   tr3.selected('MembraneSynth');
   tr4.selected('PluckSynth');
 
-
-  gen_button = createButton('⚡');
-  gen_button.position(996, 180);
-  gen_button.style('background-color', '#02c39a');
-  gen_button.style('border-radius' , 18 + '%');
-  gen_button.style('z-index',  100);
-  gen_button.style('height', 30 + 'px');
-  gen_button.mousePressed (function() {
-    clear()
-
-    stop_aud();
-    phaseShiftAmount = parseInt(phaseShiftAmountInp.value());                   // How many pulses is each shift
-    phaseShiftPeriod = parseInt(phaseShiftAmountInp.value());                   // After how many bars does a shift occur
-    length = parseInt(lengthInp.value());                            // Length of total piece
-    numberOfTracks = parseInt(numberOfTracksInp.value());
-    onsetsA = parseInt(inp.value());
-    onsetsB = parseInt(inp3.value());
-    pulsesA = parseInt(inp2.value());
-    pulsesB = parseInt(inp4.value());
-    generateMidi(onsetsA, pulsesA, onsetsB, pulsesB);
-
-    gen_button.style('background-color', '#02139b')
-  });
-
-  gen_button.mouseReleased(function () {gen_button.style('background-color', '#02c39a')});
-
-  play_button= createButton('&#9654');
-  play_button.style('width', 30 + 'px')
-  play_button.style('height', 30 + 'px')
-
-  play_button.position(100, 150);
+  // ---- Play button
+  play_button = createButton('&#9654');
+  play_button.position(6*w/12, h/3);
+  play_button.style('background-color', '#878F9B');
+  play_button.style('border-color', '#878F9B');
+  play_button.style('border-radius' , 10 + '%');
   play_button.mousePressed(start_aud_gui);
-  /*
-  play_button.style('width', 0);
-  play_button.style('height', 0);
-  play_button.style('border-top', 25 +"px solid #141430");
-  play_button.style('border-left', 10+"px solid #555");
-  play_button.style('border-bottom', 25+"px solid #141430");
-   */
 
-  stop_button= createButton('&#9982');
-  stop_button.position(130, 150);
+  // ---- Stop button
+  stop_button = createButton('&#9982');
+  stop_button.position(8*w/12, h/3);
+  stop_button.style('background-color', '#878F9B');
+  stop_button.style('border-color', '#878F9B');
+  stop_button.style('border-radius' , 10 + '%');
   stop_button.mousePressed(stop_aud);
 
-  stop_button.style('width', 30 + 'px')
-  stop_button.style('height', 30 + 'px')
-
-
-  let inp = createInput(onsetsA.toString());
-  inp.position(1000, 10);
-  inp.size(20);
-
+// ----- Inputs of the Onsets and Pulses of the Euclidean Rhythm
+  let inp1 = createInput(onsetsA.toString());
+  inp1.position(2.6*w/12, 2*h/10+30);
+  inp1.size(100);
 
   let inp2 = createInput(pulsesA.toString());
-  inp2.position(1000, 30);
-  inp2.size(20);
+  inp2.position(2.6*w/12, 2*h/10+50);
+  inp2.size(100);
 
   let inp3 = createInput(onsetsB.toString());
-  inp3.position(1000, 50);
-  inp3.size(20);
+  inp3.position(5.7*w/12, 2*h/10+30);
+  inp3.size(100);
 
   let inp4 = createInput(pulsesB.toString());
-  inp4.position(1000, 70);
-  inp4.size(20);
+  inp4.position(5.7*w/12, 2*h/10+50);
+  inp4.size(100);
 
   let phaseShiftAmountInp = createInput(phaseShiftAmount.toString());
   phaseShiftAmountInp.position(1000, 90);
@@ -138,93 +133,103 @@ function setup() {
 }
 
 function draw() {
-  background('#141430')
+
+  background('#4F5D75');
+  //background('#141430')
   //background(bg)
-  textSize(20);
+  fill('#FFFFFF');
+  textSize(40);
   textFont('Papyrus')
-  fill('#0');
-  text('1st Track', 5, 18);
 
-
+  text('1st Track', w/12, h/10);
   textSize(20);
-  text('2nd Track', 5, 38);
-
+  text('Onsets 1st Track', w/12, h/10+40);
+  text('Pulses 1st Track', w/12, h/10+60);
+  textSize(40);
+  text('2nd Track', 4*w/12, h/10);
   textSize(20);
-  text('3rd Track', 5, 58);
+  text('Onsets 2nd Track', 4*w/12, h/10+40);
+  text('Pulses 2nd Track', 4*w/12, h/10+60);
 
-  text('4th Track', 5, 78);
-
-  text('Onsets 1st Track', 800, 18);
-
-  text('Pulses 1st Track', 800, 38);
-
-  text('Onsets 2nd Track', 800, 58);
-
-  text('Pulses 2nd Track', 800, 78);
+  text('1st Track', 8.3*w/10, h/10);
+  text('2nd Track', 8.3*w/10, h/10+20);
+  text('3rd Track', 8.3*w/10, h/10+40);
+  text('4th Track', 8.3*w/10, h/10+60);
 
   text('Phase Shift Amount', 800, 98)
   text('Phase Shift Period', 800, 118)
   text('Piece length', 800, 138)
   text('Number of Tracks', 800, 158)
 
+  // ------- Generation of Concentric Circles
 
+  let c = w/4; // center
+  let r = w/8; // radius main circle
 
-  //background(220);
-  let c = h/2; //center
-  let r = w/8; // radious main circle
+  let proportion = 1;                // Relation between concentric circles
+  let cl1 = 'rgba(0,52,89,1)';       // color onsets Track 1
+  let cl2 = 'rgba(0,52,89,0.3)';     // color pulses Track 1
+  let cl3 = 'rgba(170, 52, 89,1)';   // color onsets Track 2
+  let cl4 = 'rgba(170, 52, 89,0.3)'; // color pulses Track 2
+
   // main circle
-  fill('#003459');
-  circle(c, c, 2*r);
+  proportion = 1;
+  TrackCircle(binaryRhythmA,pulsesA,proportion, cl1, cl2);
+  // second circle
+  proportion = 0.7;
+  TrackCircle(binaryRhythmB,pulsesB,proportion, cl3, cl4);
 
-  for (let i = 0; i < pulsesA; i ++){
-    //console.log(i)
-    let n_x = c+(r*cos(i*2*PI/pulsesA));
-    let n_y = c+(r*sin(i*2*PI/pulsesA));
-    fill(255,255,255);
+  function TrackCircle(onset,pulses,proportion,color1,color2) {
+    stroke(color1);
+    noFill();
+    circle(c, c, 2 * r * proportion);
+    for (let i = 0; i < pulses; i++) {
+      let n_x = c + (proportion * r * cos(i * 2 * PI / pulses));
+      let n_y = c + (proportion * r * sin(i * 2 * PI / pulses));
 
-    circle(n_x,n_y, c/10);
-    if (binaryRhythmA[i]==1){
-      fill('#02c39a');
-      circle(n_x,n_y, c/10);
+      if (onset[i] == 1) {
+        stroke(color1);
+        fill(color1);
+        circle(n_x, n_y, c / 10);
+      } else {
+        stroke(color2);
+        fill(color2);
+        circle(n_x, n_y, c / 10);
+      }
     }
   }
 
-  // Second circle
-  fill('#028090');
-  circle(c, c, 2*r*0.7);
+  proportion = 1;
+  ShuffleCircle(binaryRhythmA,pulsesA,proportion,cl1,cl2)
 
-  for (let i = 0; i < pulsesB; i ++){
-    let n_x = c+(r*0.7*cos(i*2*PI/pulsesB));
-    let n_y = c+(r*0.7*sin(i*2*PI/pulsesB));
+  proportion = 0.7;
+  ShuffleCircle(binaryRhythmB,pulsesB,proportion,cl3,cl4)
 
-    fill(255,255,255);
-    circle(n_x,n_y, c/10);
-    if (binaryRhythmB[i]==1){
-      fill('#fce38a');
-      circle(n_x,n_y, c/10);
+  function ShuffleCircle(onset,pulses,prt,color1,color2){
+    noStroke();
+
+    let x = 6*h/4;
+    let y = w/4;
+    let r2 = w/4;
+
+    for(let i = 0; i < pulses; i++) {
+      if(onset[i] == 1){
+        fill(color1)
+        arc(x,y,prt*r2,prt*r2,2*PI*(1-(i+1)/pulses),2*PI*(1-i/pulses),PIE);
+        fill('#4F5D75')
+        arc(x,y,prt*r2-15,prt*r2-15,0,2*PI,PIE);
+      }else{
+        fill(color2)
+        arc(x,y,r2*prt,r2*prt,2*PI*(1-(i+1)/pulses),2*PI*(1-i/pulses),PIE);
+        fill('#4F5D75')
+        arc(x,y,prt*r2-15,prt*r2-15,0,2*PI,PIE)
+      }
     }
   }
-
-  // third circle
-/*
-  fill('#003459');
-  circle(c, c, 2*r*0.4);
-
-  for (let i = 0; i < pulses_2; i ++){
-    let n_x = c+(r*0.4*cos(i*2*PI/pulses_2));
-    let n_y = c+(r*0.4*sin(i*2*PI/pulses_2));
-
-    fill(255,255,255);
-    circle(n_x,n_y, c/10);
-    if (onset_3[i]==1){
-      fill('#02c39a');
-      circle(n_x,n_y, c/10);
-    }
-  }*/
 }
-
-function start_aud_gui(){
-  SynthTypes = [tr1.value(),tr2.value(),tr3.value(),tr4.value()];
+function start_aud_gui() {
+  SynthTypes = [tr1.value(), tr2.value(), tr3.value(), tr4.value()];
   console.log('audio started');
   start_aud();
 }
+
