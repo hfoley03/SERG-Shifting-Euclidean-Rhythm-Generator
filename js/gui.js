4c6615ee16da9d3a72dd15f2f59826a15eafbd41
Sebastian Mendez <jsmendez_24@hotmail.com>
2023-01-15 23:28:21 +0100

    * Tutorial button modified

--- js/gui.js
+++ js/gui.js
@@ -202,13 +202,7 @@ function setup() {
   Mute4 = createCheckbox("Checkbox", 29.5*w/60, 54*h/60, w/60, w/60);
   Mute4.setStyle({rounding: 5, trackWidth: 0.1});
 
-
-  tutorial_button = createButton("?");
-  tutorial_button.position(10,10);
-  tutorial_button.mousePressed(toggleTutorial);
-
-
-
+  tutorial_button = createButton("?",55*w/60, 5*h/60,2*w/60,2*h/60);
 
   //console.log(finalMidiObject.tracks)
   //console.log("Shift_binary1 : "+GetBinaryShiftedOnset(1));
